65305f25f76772772be3327aced2e938a979bae6
Noppesj <nopain_@hotmail.com>
2014-12-04 23:20:14 +0100

    Tile

---
+++ TMXParser/Tile.h
@@ -0,0 +1,19 @@
+#ifndef TILE_H_
+#define TILE_H_
+
+enum TILE_TYPE { UNKNOWN, STATIC, ANIMATED };
+
+class TMX_Tile
+{
+	private:
+		unsigned int m_TileID;
+		unsigned int m_TileHeight;
+		unsigned int m_TileWidth;
+
+		TILE_TYPE m_TileType;
+	public:
+		TMX_Tile();
+		TMX_Tile(unsigned int tileID, unsigned int height, unsigned int width, TILE_TYPE type);
+		~TMX_Tile();
+};
+#endif
