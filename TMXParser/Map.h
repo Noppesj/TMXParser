4167c75c82e7c363bf7598d931959f516d370fa6
Noppesj <nopain_@hotmail.com>
2014-12-13 11:27:58 +0100

    1

--- TMXParser/Map.h
+++ TMXParser/Map.h
@@ -23,30 +23,23 @@ class TMX_Map
 		std::unordered_map<unsigned int, TMX_Tile> m_StaticTiles;
 		std::unordered_map<unsigned int, TMX_Tile> m_AnimatedTiles;
 
-		std::vector<int> m_TilesData;
-	public:
-		TMX_Map();
-		TMX_Map(unsigned int mapWidth, unsigned int mapHeight, std::string mapName,
-				std::string tileSetName, std::string imageName, 
-				unsigned int imageWidth, unsigned int imageHeight);
-
-		virtual ~TMX_Map();
+		int *m_TilesData;
+		unsigned int m_Size;
 
 		void addStaticTile(TMX_Tile& tile);
 		void addAnimatedTile(TMX_AnimatedTile& tile);
 		bool isAnimatedTileLoaded(unsigned int tileID);
 		bool isStaticTileLoaded(unsigned int tileID);
-		void addTilesData(int data);
+		void addTilesData(int data, unsigned int index);
+		void init(unsigned int size);
 
-		void setMapWidth(const unsigned int width){ m_MapWidth = width; };
-		void setMapHeight(const unsigned int height){ m_MapHeight = height; };
-		void setMapName(const std::string &name){ m_MapName = name; };
-		void setTileWidth(const unsigned int tileWidth){ m_TileWidth = tileWidth; };
-		void setTileHeight(const unsigned int tileHeight){ m_TileHeight = tileHeight; };
-		void setTileSetName(const std::string &tileSetName){ m_TileSetName = tileSetName; };
-		void setImageName(const std::string &imageName){ m_ImageName = imageName; };
-		void setImageWidth(const unsigned int imageWidth){ m_ImageWidth = imageWidth; };
-		void setImageHeight(const unsigned int imageHeight){ m_ImageHeight = imageHeight; };
+		int getInt(const std::string &line);
+		std::string getString(const std::string &line);
+		void readData(std::ifstream &reader);
+
+	public:
+		TMX_Map();
+		virtual ~TMX_Map();
 
 		const unsigned int getMapWidth(){ return m_MapWidth; };
 		const unsigned int getMapHeight(){ return m_MapHeight; };
@@ -57,6 +50,10 @@ class TMX_Map
 		const std::string& getImageName(){ return m_ImageName; };
 		const unsigned int getImageWidth(){ return m_ImageWidth; };
 		const unsigned int getImageHeight(){ return m_ImageHeight; };
+
+		void printTilesData();
+		bool parse(const char *tmxFile_path);
+		bool write(const char *bin_path);
 };
 
 #endif
